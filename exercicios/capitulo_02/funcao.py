652fc9321e22587ce7f651b26b042591d65aa163
Andreus265 <andre.ribs265@gmail.com>
2024-07-12 14:33:30 +0000

    resolução

---
+++ exercicios/capitulo_02/funcao.py
@@ -0,0 +1,15 @@
+def funcao(a):
+    resultado = a
+    return resultado
+
+resultado_var = funcao(7)
+print('o valor da função é:', resultado_var)
+
+
+def funcao2(a, b):
+    resultado2 = a + b
+    return resultado2
+
+resultado2_var = funcao2(3, 4)
+print('o valor da função é:', resultado2_var)
+
